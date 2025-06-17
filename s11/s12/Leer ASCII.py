9279aa9b1844a3ba9ee7dd8df137371e02589a4f
andrev3214 <avbermudez@uamv.edu.ni>
2025-06-17 13:11:21 -0600

    Delete s11/s12 directory

--- s11/s12/Leer ASCII.py
+++
@@ -1,9 +0,0 @@
-# Programa para mostrar archivo ASCII
-
-arch1 = open("ascii.txt", "r", encoding="utf-8")
-print("Contenido del archivo ASCII:")
-
-for linea in arch1:
-    print(linea, end="")
-    
-arch1.close()
